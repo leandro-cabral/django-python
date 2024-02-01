69bfc2d3fd3fbb4aaa902b7447e64642cd0924cf
Leandro Cabral <leandrocabralalmeida@gmail.com>
2024-02-01 15:21:38 -0300

    First commit

---
+++ learning_log/ll_project/learning_logs/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """Um tópico que o usuário está aprendendo"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Retorna uma apresentação de string modelo"""
+        return self.text
+
+class Entry(models.Model):
+    """Algo específico aprendido sobre um tópico"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Retorna uma string simples representando a entrada"""
+        return f"{self.text[:50]}..."
