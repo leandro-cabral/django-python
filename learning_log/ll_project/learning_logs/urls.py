0902e76c235dfd53140bf608e2a22363b18a7dd0
Leandro Cabral <leandrocabralalmeida@gmail.com>
2024-02-01 18:53:10 -0300

    .

---
+++ learning_log/ll_project/learning_logs/urls.py
@@ -0,0 +1,16 @@
+"""Define padrões de URL para learning_logs"""
+from django.urls import path
+from . import views 
+
+app_name = 'learning_logs'
+urlpatterns = [ 
+    # Página inicial
+    path('', views.index, name='index'),
+
+    # Página que mostra todos os tópicos 
+    path('topics', views.topics, name='topics'),
+
+    # Página de detalhes para um único tópico
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+]
